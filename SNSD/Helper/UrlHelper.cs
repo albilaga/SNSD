a3f5c7fd92f532dda0abdc58955435ce29e5fb89
albilaga <albilaga@ALBILAGALIN9433>
2016-05-25 00:14:17 +0700

    change base api url

--- SNSD/Helper/UrlHelper.cs
+++ SNSD/Helper/UrlHelper.cs
@@ -2,7 +2,7 @@ namespace SNSD.Helper
 {
     public static class UrlHelper
     {
-        public static string BASE_URL = "http://bharabas.azurewebsites.net/Api/";
+        public static string BASE_URL = "http://snsd.azurewebsites.net/Api/";
 
         public static string LIST_MEMBERS = BASE_URL + "Members";
     }
